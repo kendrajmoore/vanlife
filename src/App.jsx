93e1bbd02290696df7f8581daaeb8e99b0984b91
kendrajmoore <kendra.j.moore3443@gmail.com>
2023-11-05 18:05:29 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import About from "./components/About.jsx"
-import Home from "./components/Home.jsx"
+import About from "./components/About"
+import Home from "./components/Home"
+import Vans from "./components/Vans"
+import VanDetail from './components/VanDetail';
+import Layout from './components/Layout';
+import Dashboard from './components/Dashboard'
+import Reviews from './components/Reviews'
+import Income from './components/Income'
+import HostLayout from './components/HostLayout';
+
+import "./server"
 import './App.css'
 
 function App() {
@@ -11,17 +20,20 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        <header>
-          <nav>
-            <Link to="/">#VANLIFE</Link>
-            <Link to="about">About</Link>
-          </nav>
-        </header>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
+          <Route element={<Layout/>}>
+            <Route index element={<Home/>} />
+            <Route path="about" element={<About/>} />
+            <Route path="vans" element={<Vans/>} />
+            <Route path="vans/:id" element={<VanDetail />} />
+            <Route path="/host" element={<HostLayout/>}>
+              <Route index element={<Dashboard/>}/>
+              <Route path="income" element={<Income/>} />
+              <Route path="reviews" element={<Reviews/>} />
+            </Route>
+          </Route>
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>&#169; 2023 #VANLIFE</footer>
+  )
+}
+
+export default Footer;
